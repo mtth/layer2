c284f5f93167b02b1927339e753b00c9470f704d
Matthieu Monsch <monsch@alum.mit.edu>
2015-01-04 11:47:29 -0800

    Add snapshot example.

---
+++ examples/snapshot.js
@@ -0,0 +1,32 @@
+/* jshint node: true */
+
+/**
+ * Save a 5 second snapshot of live frames.
+ *
+ * We create a Live stream from the default network interface and store 5
+ * seconds' worth of frames to a file by writing to a Save stream. We also
+ * print each frame's length and the total number of frames processed at the
+ * very end.
+ */
+(function () {
+  'use strict';
+
+  var dot11 = require('../src/js');
+
+  if (process.argv.length != 3) {
+    console.error('Usage: node snapshot.js PATH');
+    process.exit(1);
+  }
+
+  var fpath = process.argv[2];
+  var liveStream = new dot11.capture.Live();
+  var saveStream = new dot11.capture.Save(fpath);
+
+  liveStream
+    .close(5000)
+    .on('end', function () {
+      console.log('Saved ' + this.getStats().psRecv + ' frames!');
+    })
+    .pipe(saveStream);
+
+})();
