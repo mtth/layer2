2d646112d390435154e77ba0ad414597f28ffc2b
Matthieu Monsch <monsch@alum.mit.edu>
2015-01-25 14:53:08 -0800

    Add Frame to package top level.

--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,12 @@
 (function (root) {
   'use strict';
 
+  var addon = require('./utils').requireAddon();
+
   root.exports = {
     capture: require('./capture'),
-    Decoder: require('./decode').Decoder
+    Decoder: require('./decode').Decoder,
+    Frame: addon.Frame
   };
 
 })(module);
