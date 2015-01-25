c99e853660cdf65ce583fe3d65745d47039edd5a
Matthieu Monsch <monsch@alum.mit.edu>
2015-01-25 15:26:50 -0800

    Implement frame inspect method.

--- src/js/decode.js
+++ src/js/decode.js
@@ -20,6 +20,96 @@
       addon = require('./utils').requireAddon();
 
   /**
+   * A frame, magic!
+   *
+   */
+  var Frame = addon.Frame;
+
+  Frame.prototype.inspect = function () {
+
+    var pduNames = [];
+    var pduTypes = this.getPduTypes();
+    var l = pduTypes.length;
+    var i;
+    if (l > 0) {
+      // Valid frame.
+      for (i = 0; i < l; i++) {
+        pduNames.push(Frame.getPduName(pduTypes[i]) || '?');
+      }
+      return '<Frame ' + pduNames.join(' | ') + '>';
+    } else {
+      // Invalid frame.
+      return '<Frame !>';
+    }
+
+  };
+
+  /**
+   * Get a name from a PDU type.
+   *
+   */
+  Frame.getPduName = (function () {
+
+    // https://github.com/mfontanini/libtins/blob/master/include/tins/pdu.h
+    var pdus = [
+      'RAW',
+      'ETHERNET_II',
+      'IEEE802_3',
+      'RADIOTAP',
+      'DOT11',
+      'DOT11_ACK',
+      'DOT11_ASSOC_REQ',
+      'DOT11_ASSOC_RESP',
+      'DOT11_AUTH',
+      'DOT11_BEACON',
+      'DOT11_BLOCK_ACK',
+      'DOT11_BLOCK_ACK_REQ',
+      'DOT11_CF_END',
+      'DOT11_DATA',
+      'DOT11_CONTROL',
+      'DOT11_DEAUTH',
+      'DOT11_DIASSOC',
+      'DOT11_END_CF_ACK',
+      'DOT11_MANAGEMENT',
+      'DOT11_PROBE_REQ',
+      'DOT11_PROBE_RESP',
+      'DOT11_PS_POLL',
+      'DOT11_REASSOC_REQ',
+      'DOT11_REASSOC_RESP',
+      'DOT11_RTS',
+      'DOT11_QOS_DATA',
+      'LLC',
+      'SNAP',
+      'IP',
+      'ARP',
+      'TCP',
+      'UDP',
+      'ICMP',
+      'BOOTP',
+      'DHCP',
+      'EAPOL',
+      'RC4EAPOL',
+      'RSNEAPOL',
+      'DNS',
+      'LOOPBACK',
+      'IPv6',
+      'ICMPv6',
+      'SLL',
+      'DHCPv6',
+      'DOT1Q',
+      'PPPOE',
+      'STP',
+      'PPI',
+      'IPSEC_AH',
+      'IPSEC_ESP',
+      'PKTAP'
+    ];
+
+    return function (pduType) { return pdus[pduType]; };
+
+  })();
+
+  /**
    * Decoder stream class.
    *
    * This class will "dynamically" look up the correct transform function based
@@ -84,7 +174,8 @@
   util.inherits(Decoder, stream.Transform);
 
   root.exports = {
-    Decoder: Decoder
+    Decoder: Decoder,
+    Frame: Frame
   };
 
 })(module);
