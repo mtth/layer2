4d3757ad2071e74f3563ccae6b4787c4d55072bf
Matthieu Monsch <monsch@alum.mit.edu>
2015-01-25 14:48:10 -0800

    Idem.

---
+++ scripts/install_libtins.sh
@@ -0,0 +1,18 @@
+#!/bin/env bash
+set -e
+
+libtins_v=3.1
+
+mkdir tmp
+cd tmp
+curl -L "https://github.com/mfontanini/libtins/archive/v${libtins_v}.zip" >libtins.zip
+unzip libtins.zip
+cd "libtins-${libtins_v}"
+mkdir build
+cd build
+cmake ../
+make
+sudo make install
+cd ../../..
+rm -r tmp
+npm install
--- scripts/install_linux.sh
+++
@@ -1,9 +0,0 @@
-sudo apt-get -qq update
-sudo apt-get install libpcap-dev libssl-dev cmake
-git clone git@github.com:mfontanini/libtins.git
-cd libtins
-mkdir build
-cd build
-cmake ../
-make
-sudo make install
